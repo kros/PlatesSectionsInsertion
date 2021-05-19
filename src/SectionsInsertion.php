91a1344e343c6e9ec44e1198dcce0586777a9fdc
Oscar Ruiz <ormartin@gmail.com>
2021-05-19 11:43:22 +0200

    Update SectionsInsertion.php

--- src/SectionsInsertion.php
+++ src/SectionsInsertion.php
@@ -14,7 +14,8 @@ class SectionsInsertion implements ExtensionInterface{
 	{
 		$this->engine = $engine;
 		$this->engine->registerFunction('setSectionContent', [$this, 'setSectionContent']);
-
+		$this->engine->registerFunction('pushSectionContent', [$this, 'pushSectionContent']);
+		$this->engine->registerFunction('unshiftSectionContent', [$this, 'unshiftSectionContent']);
 	}
 	
 	/**
@@ -29,4 +30,30 @@ class SectionsInsertion implements ExtensionInterface{
 		$this->template->insert($templateName, $params); 
 		$this->template->stop();
 	}
+
+	/**
+	* Push the content of a template into a section.
+	*
+	* @param $sectionName Name of the section
+	* @param $templateName Name of the template
+	* @param $params Optional. Array of params to be passed to the template, if needed.
+	*/
+	public function pushSectionContent($sectionName, $templateName, $params=[]){
+		$this->template->push($sectionName); 
+		$this->template->insert($templateName, $params); 
+		$this->template->stop();
+	}
+
+	/**
+	* Unshift the content of a template into a section.
+	*
+	* @param $sectionName Name of the section
+	* @param $templateName Name of the template
+	* @param $params Optional. Array of params to be passed to the template, if needed.
+	*/
+	public function unshiftSectionContent($sectionName, $templateName, $params=[]){
+		$this->template->unshift($sectionName); 
+		$this->template->insert($templateName, $params); 
+		$this->template->stop();
+	}	
 }
