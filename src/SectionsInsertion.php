b73f5cf7c624584a93f4c201639e5cbc8a84f641
Oscar Ruiz <ormartin@gmail.com>
2021-05-19 11:22:15 +0200

    Update SectionsInsertion.php

--- src/SectionsInsertion.php
+++ src/SectionsInsertion.php
@@ -7,13 +7,23 @@ use League\Plates\Extension\ExtensionInterface;
 class SectionsInsertion implements ExtensionInterface{
 	public $template;
 	protected $engine;
+	/**
+	* @param \League\Plates\Engine $engine
+	*/
 	public function register(Engine $engine)
 	{
 		$this->engine = $engine;
-		
 		$this->engine->registerFunction('setSectionContent', [$this, 'setSectionContent']);
 
 	}
+	
+	/**
+	* Insert the content of a template into a section.
+	*
+	* @param $sectionName Name of the section
+	* @param $templateName Name of the template
+	* @param $params Optional. Array of params to be passed to the template, if needed.
+	*/
 	public function setSectionContent($sectionName, $templateName, $params=[]){
 		$this->template->start($sectionName); 
 		$this->template->insert($templateName, $params); 
